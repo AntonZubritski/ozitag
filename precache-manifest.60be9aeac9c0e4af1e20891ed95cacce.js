f20368c2210e360ffe663be96ee441f1c3faa4ad
AntonZubritski <a.zubritski@yandex.ru>
2020-09-03 17:49:51 +0300

    Updates

---
+++ precache-manifest.60be9aeac9c0e4af1e20891ed95cacce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c0e508cc6264178f260621b3c4b0f4",
+    "url": "/ozitag/index.html"
+  },
+  {
+    "revision": "151137497f7fa5cfd42b",
+    "url": "/ozitag/static/css/main.28a8469b.chunk.css"
+  },
+  {
+    "revision": "c50e246291394fda2e96",
+    "url": "/ozitag/static/js/2.091dea73.chunk.js"
+  },
+  {
+    "revision": "9d36fd18d7d7b7370f96c95702a6c2fd",
+    "url": "/ozitag/static/js/2.091dea73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151137497f7fa5cfd42b",
+    "url": "/ozitag/static/js/main.7b6e257a.chunk.js"
+  },
+  {
+    "revision": "90013b935e07402d93fb",
+    "url": "/ozitag/static/js/runtime-main.33541cdc.js"
+  }
+]);
